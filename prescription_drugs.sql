285a40ff69cfa8b4b48702fc8f571ddf7fd98b59
Brooke Jackson <brooke.m.jackson23@gmail.com>
2023-02-08 15:30:32 -0600

    initial push

--- prescription_drugs.sql
+++ prescription_drugs.sql
@@ -2,24 +2,27 @@
 
 --1. a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
-SELECT total_claim_count, nppes_provider_last_org_name, npi
+SELECT SUM(total_claim_count) AS total_claims, npi
 FROM prescription
 LEFT JOIN prescriber
 USING(npi)
-ORDER BY total_claim_count DESC
+GROUP BY npi
+ORDER BY total_claims DESC
 LIMIT 1;
 
---Answer: Coffey, 4538, 1912011792
+--Answer: 99707 claims and npi is 1881634483.
 	
 --b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
 
-SELECT nppes_provider_last_org_name, nppes_provider_first_name, total_claim_count, p1.specialty_description, p1.npi
-FROM prescriber AS p1
-INNER JOIN prescription AS p2
-ON p1.npi = p2.npi
-ORDER BY total_claim_count DESC
+SELECT SUM(total_claim_count) AS total_claims, npi, nppes_provider_last_org_name, nppes_provider_first_name, specialty_description
+FROM prescription
+LEFT JOIN prescriber
+USING(npi)
+GROUP BY npi, nppes_provider_last_org_name, nppes_provider_first_name, specialty_description
+ORDER BY total_claims DESC
+LIMIT 1;
 
---Answer: David Coffey, 4538, Family Practice
+--Answer: Bruce Pendley, Family Practice, with total claims as 99707 and npi is 1881634483.
 
 --2. a. Which specialty had the most total number of claims (totaled over all drugs)?
 
@@ -28,22 +31,23 @@ FROM prescriber AS p1
 INNER JOIN prescription AS p2
 ON p1.npi = p2.npi
 GROUP BY specialty_description
-ORDER BY SUM(total_claim_count) DESC
+ORDER BY SUM(total_claim_count) DESC;
 
---Answer: Family Practice
+--Answer: Family Practice had 9,752,347 claims.
 
 --b. Which specialty had the most total number of claims for opioids?
 
-SELECT DISTINCT drug.drug_name, prescription.total_claim_count, specialty_description
-FROM drug
-INNER JOIN prescription
-ON drug.drug_name = prescription.drug_name
+SELECT SUM(total_claim_count) AS claim_amount, specialty_description
+FROM prescription
 INNER JOIN prescriber
-ON prescriber.npi = prescription.npi
-WHERE opioid_drug_flag = 'Y'
-ORDER BY total_claim_count DESC
+USING(npi)
+INNER JOIN drug
+USING(drug_name)
+WHERE opioid_Drug_flag = 'Y'
+GROUP BY specialty_description
+ORDER BY claim_amount DESC;
 
---Answer: Family Practice
+--Answer: Nurse Practitioner with 900,845
 
 --c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
 
@@ -51,24 +55,25 @@ ORDER BY total_claim_count DESC
 
 --3. a. Which drug (generic_name) had the highest total drug cost?
 
-SELECT DISTINCT generic_name, total_drug_cost
+SELECT SUM(total_drug_cost) AS drug_cost, generic_name
 FROM drug
-INNER JOIN prescription 
-ON drug.drug_name = prescription.drug_name
-ORDER BY total_drug_cost DESC
+INNER JOIN prescription
+USING(drug_name)
+GROUP BY generic_name
+ORDER BY drug_cost DESC;
 
---Answer: PIRFENIDONE where cost = 2829174.3
+--Answer: Insulin with cost $104,264,066.35
 
 --b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
 
-SELECT DISTINCT generic_name, (SUM(total_drug_cost) / SUM(total_30_day_fill_count)) AS pres_cost
+SELECT ROUND(SUM(total_drug_cost) / 365,2) AS pres_cost, generic_name
 FROM drug
 INNER JOIN prescription 
-ON drug.drug_name = prescription.drug_name
+USING(drug_name)
 GROUP BY generic_name
 ORDER BY pres_cost DESC
 
---Answer: Chenodiol with 86741
+--Answer: Insulin with $285,654.98
 
 --4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
@@ -80,7 +85,7 @@ FROM drug;
 
 --b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
-SELECT SUM(DISTINCT total_drug_cost) AS money,
+SELECT SUM(total_drug_cost) AS money,
 CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid' 
 	WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
 	ELSE 'neither' END AS drug_type
